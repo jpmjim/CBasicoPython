dc222b734756819b38f1be2fddb7e09dc38e531d
Jimmy Joel P. Malqui <jpmjim@gmail.com>
2021-10-28 20:36:20 -0500

    Recorriendo un string con for

---
+++ 7recorrer.py
@@ -0,0 +1,14 @@
+def run():
+  # Imprimir letra 
+  # nombre = input('Esxribe tu nombre: ')
+  # for letra in nombre:
+  #   print(letra)
+
+  # Imprimir caracter cada uno en mayusculas
+  frase = input('Escribe una frase: ')
+  for caracter in frase:
+    print(caracter.upper())
+
+
+if __name__ == '__main__':
+  run()
