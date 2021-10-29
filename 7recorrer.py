35ec7222955df285803bb88f040205fe3ba27712
Jimmy Joel P. Malqui <jpmjim@gmail.com>
2021-10-28 20:50:01 -0500

    Interrumpiendo ciclos con break y continue

--- 7recorrer.py
+++ 7recorrer.py
@@ -1,8 +1,8 @@
 def run():
   # Imprimir letra 
-  # nombre = input('Esxribe tu nombre: ')
-  # for letra in nombre:
-  #   print(letra)
+  nombre = input('Esxribe tu nombre: ')
+  for letra in nombre:
+    print(letra)
 
   # Imprimir caracter cada uno en mayusculas
   frase = input('Escribe una frase: ')
---
+++ 8break_continue.py
@@ -0,0 +1,21 @@
+def run():
+  # imprimir numeros pares con continue
+  for contador in range(1000):
+    if contador % 2 != 0:
+      continue
+    print(contador)
+  
+  # for con break
+  for i in range(10000):
+    print(i)
+    if i == 5678:
+      break
+
+  texto = input('Escribe un texto: ')
+  for letra in texto:
+    if letra == 'o':
+      break
+    print(letra)
+
+if __name__ == '__main__':
+  run()
