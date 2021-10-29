c2e8b4c72aa977318de52abce28336524d694f88
Jimmy Joel P. Malqui <jpmjim@gmail.com>
2021-10-28 20:26:22 -0500

    Bucles, El ciclo for

---
+++ 6for.py
@@ -0,0 +1,13 @@
+# contador con bucle while
+contador = 1
+print(contador)
+while contador < 1000:
+  contador += 1 
+  print(contador)
+
+# contador con bucle for
+for contador in range(1, 1001):
+  print(contador)
+
+for i in range(10):
+  print(11 * i)
