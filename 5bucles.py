5ecd275cac155e3e5a45e91709048c03e3eca000
Jimmy Joel P. Malqui <jpmjim@gmail.com>
2021-10-28 20:11:25 -0500

    Bucles, El ciclo while

---
+++ 5bucles.py
@@ -0,0 +1,14 @@
+# El ciclo while
+def run():
+  LIMITE = 1000000 # es una constante
+
+  contador = 0
+  potencia_2 = 2**contador
+  while potencia_2 < LIMITE:
+    print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
+    contador = contador + 1
+    potencia_2 = 2**contador
+
+
+if __name__ == '__main__':
+  run()
